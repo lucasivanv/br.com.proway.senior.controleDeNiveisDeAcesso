8df292daeb5659c2b2e10ec73165131b18301bb1
Lucas Ivan <SENIOR@proway.treina>
2021-05-04 15:16:54 -0300

    Refatorado UsuarioController com DB

--- src/main/java/controller/UsuarioController.java
+++ src/main/java/controller/UsuarioController.java
@@ -7,19 +7,23 @@ import java.util.regex.Pattern;
 
 import model.acesso.HashSenha;
 import model.acesso.PerfilModel;
+import model.acesso.PermissaoModel;
 import model.acesso.UsuarioModel;
 import model.acesso.UsuarioDAO;
 import model.interfaces.InterfaceUsuarioController;
 
 /**
+ * Classe UsuarioController
  * 
- * @author Vitor Peres
- * @author David Willian
- * @author Leonardo Pereira
- * vitor.peres@senior.com.br
- * leonardo.pereira@senior.com.br
- * david.oliveira@senior.com.br
+ * Classe responsável pelas validações e verificações das entradas e saídas
  * 
+ * @author Sprint 3
+ * @author David Willian, david.oliveira@senior.com.br
+ * @author Leonardo Pereira, leonardo.pereira@senior.com.br
+ * @author Vitor Peres, vitor.peres@senior.com.br
+ * 
+ * @author Sprint 4
+ * @author Lucas Ivan, lucas.ivan@senior.com.br
  */
 public class UsuarioController implements InterfaceUsuarioController {
 
@@ -37,8 +41,7 @@ public class UsuarioController implements InterfaceUsuarioController {
 	 * @param String email
 	 * @return isValidaEmail
 	 * 
-	 */
-	
+	 */	
 	public boolean validarEmail(String email) {
 		boolean emailValido = false;
 		if (email != null && email.length() > 0) {
@@ -90,12 +93,156 @@ public class UsuarioController implements InterfaceUsuarioController {
 	 * @return boolean
 	 */
 	public boolean verificarHashSenha(Integer usuarioID, String senha) {		
-		if(this.converterSenhaEmHashSenha(senha).equals(daoUsuario.get(usuarioID).getHashSenhaDoUsuario())) {
+	
+		if(this.converterSenhaEmHashSenha(senha).equals(daoUsuario.buscarUsuario(usuarioID).getHashSenhaDoUsuario())) {
+			return true;
+		}
+		return false;
+	}	
+	
+	/**
+	 * Método que altera a senha do usuario
+	 * 
+	 * Verifica se a nova senha atende os critérios de senha segura e se a nova senha é diferente da senha atual.
+	 * Recebe id e nova senha para alterar
+	 * 
+	 * @param int id do usuario procurado para alteraçao
+	 * @param String senhaNova do usuario
+	 */
+	public boolean alteraSenha(Integer usuarioID, String senhaNova) {
+
+		if (this.validarSenha(senhaNova) && !this.verificarHashSenha(usuarioID, senhaNova)) {
+
+			daoUsuario.buscarUsuario(usuarioID).setHashSenhaDoUsuario(this.converterSenhaEmHashSenha(senhaNova));
+
+			daoUsuario.atualizarUsuario(usuarioID, daoUsuario.buscarUsuario(usuarioID));
 			return true;
 		}
 		return false;
 	}
+	
+	/**
+	 * Método que altera o login do usuario
+	 * 
+	 * Recebe id e novo login para alterar
+	 * 
+	 * @param int id do usuario procurado para alteraçao
+	 * @param String loginNovo do usuario
+	 */
+	public boolean alteraLogin(Integer usuarioID, String loginNovo) {
+		
+		if(this.validarEmail(loginNovo)) {
 
+			if(!daoUsuario.buscarUsuario(usuarioID).getLoginDoUsuario().equals(loginNovo)){
+				
+				daoUsuario.buscarUsuario(usuarioID).setHashSenhaDoUsuario(loginNovo);
+				
+				daoUsuario.atualizarUsuario(usuarioID, daoUsuario.buscarUsuario(usuarioID));
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Método que altera perfil do usuario.
+	 * 
+	 * Recebe id do usuario, e perfil novo para alterar
+	 * 
+	 * @param int id do usuario procurado para a troca de perfil
+	 * @param Perfil perfilNovo do usuario que vai ter o perfil trocado.
+	 */
+	
+//	@Deprecated
+//	public void alteraPerfil(int id, ArrayList<PerfilModel> listaPerfil) {
+//	
+//		UsuarioModel usuarioEscolhido = daoUsuario.get(id);
+//		usuarioEscolhido.setListaDePerfisDoUsuario(listaPerfil);
+//		daoUsuario.update(usuarioEscolhido);
+//	}
+	
+	/**
+	 *  Cria um usuário.
+	 *  
+	 *  Recebe as informações de um usuário, transforma a senha em hash e cria um objeto UsuarioModel
+	 *  @param idDoUsuario int
+	 *  @param loginDoUsuario String
+	 *  @param senha String
+	 *  @return boolean
+	 */
+	public boolean criarUsuarioController(Integer idDoUsuario, String loginDoUsuario, String senha) {
+				
+		for (UsuarioModel usuarioModel : daoUsuario.buscarTodosUsuarios()) {
+			if(usuarioModel.getLoginDoUsuario().equals(loginDoUsuario)) {
+				return false;
+			}
+		} 
+		daoUsuario.criarUsuario(this.converterSenhaEmHashSenha(senha), loginDoUsuario);
+		return true;
+	}
+	
+	/**
+	 *  Deleta um usuário de acordo com o login informado  
+	 *  
+	 *  @param idDoUsuario int
+	 *  @param loginDoUsuario String
+	 *  @return boolean
+	 */
+	public boolean deletarUsuarioController(Integer idDoUsuario, String loginDoUsuario) {
+		
+		for (UsuarioModel usuarioModel : daoUsuario.buscarTodosUsuarios()) {
+			if (usuarioModel.getLoginDoUsuario().equals(loginDoUsuario)) {
+				daoUsuario.deletarUsuario(idDoUsuario);
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 *  Busca um usuário de acordo com o login informado  
+	 *  
+	 *  @param idDoUsuario int
+	 *  @param loginDoUsuario String
+	 *  @return UsuarioModel
+	 */
+	public UsuarioModel buscarUsuarioController(Integer idDoUsuario, String loginDoUsuario) {
+		
+		for (UsuarioModel usuarioModel : daoUsuario.buscarTodosUsuarios()) {
+			if (usuarioModel.getLoginDoUsuario().equals(loginDoUsuario)) {
+				return daoUsuario.buscarUsuario(idDoUsuario);
+			}
+		}
+		return null;
+	}
+	
+	/**
+	 *  Busca todos os usuarios cadastrados  
+	 *  
+	 *  @return ArrayList<UsuarioModel>
+	 */
+	public ArrayList<UsuarioModel> buscarTodosUsuariosController() {
+		return daoUsuario.buscarTodosUsuarios();
+	}
+	
+	/**
+	 *  Atualiza um usuário de acordo com o login informado  
+	 *  
+	 *  @param idDoUsuario int
+	 *  @param loginDoUsuario String
+	 *  @return boolean
+	 */
+//	public boolean atualizarUsuarioController(Integer idDoUsuario, String loginDoUsuario) {
+//		
+//		for (UsuarioModel usuarioModel : daoUsuario.buscarTodosUsuarios()) {
+//			if (usuarioModel.getLoginDoUsuario().equals(loginDoUsuario)) {
+//				daoUsuario.atualizarUsuario(idDoUsuario, usuarioModel);
+//				return true;
+//			}
+//		}
+//		return false;
+//	}
+	
 	/**
 	 * Envia um e-mail
 	 * 
@@ -113,60 +260,8 @@ public class UsuarioController implements InterfaceUsuarioController {
 			return codigo;
 		}
 		return codigo;
-	}
-
-	/**
-	 * Método que altera a senha do usuario
-	 * 
-	 * Verifica se a nova senha atende os critérios de senha segura e se a nova senha é diferente da senha atual.
-	 * Recebe id e nova senha para alterar
-	 * 
-	 * @param int id do usuario procurado para alteraçao
-	 * @param String senhaNova do usuario
-	 */
-	public boolean alteraSenha(int id, String senhaNova) {
-		UsuarioModel usuarioEscolhido = daoUsuario.get(id);
-		if(this.validarSenha(senhaNova)) {
-			String senhaHash = this.converterSenhaEmHashSenha(senhaNova);
-			if(!usuarioEscolhido.getHashSenhaDoUsuario().equals(senhaHash)) {
-				usuarioEscolhido.setHashSenhaDoUsuario(senhaHash);
-				daoUsuario.update(usuarioEscolhido);
-				return true;
-			}
-		}
-		return false;
-	}
+	}	
 	
-	/**
-	 * Método que altera o login do usuario
-	 * 
-	 * Recebe id e novo login para alterar
-	 * 
-	 * @param int id do usuario procurado para alteraçao
-	 * @param String loginNovo do usuario
-	 */
-	public void alteraLogin(int id, String loginNovo) {
-		
-		UsuarioModel usuarioEscolhido = daoUsuario.get(id);
-		usuarioEscolhido.setLoginDoUsuario(loginNovo);
-		daoUsuario.update(usuarioEscolhido);
-	}
-	
-	/**
-	 * Método que altera perfil do usuario.
-	 * 
-	 * Recebe id do usuario, e perfil novo para alterar
-	 * 
-	 * @param int id do usuario procurado para a troca de perfil
-	 * @param Perfil perfilNovo do usuario que vai ter o perfil trocado.
-	 */
-	public void alteraPerfil(int id, ArrayList<PerfilModel> listaPerfil) {
-	
-		UsuarioModel usuarioEscolhido = daoUsuario.get(id);
-		usuarioEscolhido.setListaDePerfisDoUsuario(listaPerfil);
-		daoUsuario.update(usuarioEscolhido);
-	}
-
 	/**
 	 * Gera um código aleatório
 	 * 
@@ -184,21 +279,4 @@ public class UsuarioController implements InterfaceUsuarioController {
 		return true;
 	}
 	
-	/**
-	 *  Cria um usuário.
-	 *  
-	 *  Recebe as informações de um usuário, transforma a senha em hash e cria um objeto UsuarioModel
-	 *  @param idDoUsuario int
-	 *  @param loginDoUsuario String
-	 *  @param senha String
-	 *  @return UsuarioModel
-	 */
-	
-	public UsuarioModel criarUsuarioModel(int idDoUsuario, String loginDoUsuario, String senha) {
-		if(!validarSenha(senha)) {
-			return null;
-		}
-		String hashSenha = HashSenha.senhaDoUsuario(senha);
-		return new UsuarioModel(idDoUsuario, loginDoUsuario, hashSenha, new ArrayList<PerfilModel>());
-	}
 }
