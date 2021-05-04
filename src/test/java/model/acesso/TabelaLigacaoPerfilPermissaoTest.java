0023f97491903a0f1b05306c9e5c246faa9bfa80
Vitor Nathan Gonçalves <vitornathang@gmail.com>
2021-05-04 17:25:49 -0300

    Tabelas de ligação completas

--- src/test/java/model/acesso/TabelaLigacaoPerfilPermissaoTest.java
+++ src/test/java/model/acesso/TabelaLigacaoPerfilPermissaoTest.java
@@ -10,14 +10,14 @@ public class TabelaLigacaoPerfilPermissaoTest {
 	public void testPermissoesDoPerfil() {
 		TabelaLigacaoPerfilPermissao tb = new TabelaLigacaoPerfilPermissao();
 		
-		tb.adicionarLigacao(223, 65);
-		tb.adicionarLigacao(222, 69);
-		tb.adicionarLigacao(220, 67);
-		tb.adicionarLigacao(222, 70);
-		tb.adicionarLigacao(224, 70);
-		tb.adicionarLigacao(223, 70);
-		System.out.println(tb.perfisDaPermissao(70).toString());
-		System.out.println(tb.permissoesDoPerfil(222).toString());
+		tb.adicionarLigacao(65, 223);
+		tb.adicionarLigacao(69, 222);
+		tb.adicionarLigacao(67, 220);
+		tb.adicionarLigacao(70, 222);
+		tb.adicionarLigacao(70, 224);
+		tb.adicionarLigacao(70, 223);
+		System.out.println(tb.perfisDaPermissao(222).toString());
+		System.out.println(tb.permissoesDoPerfil(70).toString());
 		
 	}
 
